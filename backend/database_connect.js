3de1d084e51b7d24434a2b1ad9e1589744790dd9
WoKrishhnaHai <krishnagupta4371@gmail.com>
2023-09-23 10:24:59 +0530

    Added MONGODB Atlas

--- backend/database_connect.js
+++ backend/database_connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/chatbot').then(() => {
+    mongoose.connect('mongodb+srv://chatbotuser:chatbotuser123@chatbot.rrkujzv.mongodb.net/').then(() => {
         console.log("Connected to MongoDB!")
     });
 }
