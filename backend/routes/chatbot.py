e0cce3fe4d7427ce7e4d698b3d2cd4824289b418
PriyankGaikwad <priyank.22111319@viit.ac.in>
2023-11-23 06:50:39 +0530

    Updated ML model

--- backend/routes/chatbot.py
+++ backend/routes/chatbot.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import LabelEncoder
 import json
 import sys
 
@@ -9,29 +10,27 @@ training_set = pd.read_excel('./backend/routes/training_set.xlsx', sheet_name='S
 X_train = training_set['Questions']
 y_train = training_set['Answers']
 
+options_set = pd.read_excel('./backend/routes/options.xlsx', sheet_name='Sheet1')
+X_options = options_set['Questions']
+y_options = options_set['Answers']
+
+label_encoder = LabelEncoder()
+y_train_encoded = label_encoder.fit_transform(y_train)
+
 tfidf_vectorizer = TfidfVectorizer()
 X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-random_forest = RandomForestClassifier(n_estimators=100, random_state=42)
-random_forest.fit(X_train_tfidf, y_train)
+xgb_classifier = XGBClassifier()
+xgb_classifier.fit(X_train_tfidf, y_train_encoded)
 
-
-def generate_options(user_input, all_questions, tfidf_vectorizer):
-    
+def generate_options(user_input, options, tfidf_vectorizer):
     user_input_tfidf = tfidf_vectorizer.transform([user_input])
-
-    all_questions_tfidf = tfidf_vectorizer.transform(all_questions)
-
-    similarity_scores = cosine_similarity(user_input_tfidf, all_questions_tfidf)[0]
-
-    top_indices = similarity_scores.argsort()[-3:][::-1]
-
-    options = [all_questions[i] for i in top_indices if all_questions[i] != user_input]
-
-    remaining_options = set(all_questions) - set(options + [user_input])
-    options.extend(list(remaining_options)[:3 - len(options)])
-
-    return options[:3]
-
+    options_tfidf = tfidf_vectorizer.transform(options)
+    similarity_scores = cosine_similarity(user_input_tfidf, options_tfidf)[0]
+    top_indices = similarity_scores.argsort()[-5:-2][::-1]
+    generated_options = [options.iloc[i] for i in top_indices if options.iloc[i] != user_input]
+    remaining_options = set(options) - set(generated_options + [user_input])
+    generated_options.extend(list(remaining_options)[:3 - len(generated_options)])
+    return generated_options[:3]
 
 user_input = sys.argv[1]
 selected_option = sys.argv[2] if len(sys.argv) > 2 else None
@@ -39,18 +38,21 @@ selected_option = sys.argv[2] if len(sys.argv) > 2 else None
 if len(user_input.strip()) == 0:
     response = {"response": "Please enter a valid query."}
 else:
-    if selected_option and selected_option in X_train.tolist():
-        predicted_answer = y_train[X_train[X_train == selected_option].index[0]]
+    user_input_tfidf = tfidf_vectorizer.transform([user_input])
+    predicted_answer_encoded = xgb_classifier.predict(user_input_tfidf)[0]
+    predicted_answer = label_encoder.inverse_transform([predicted_answer_encoded])[0]
+    confidence_score = xgb_classifier.predict_proba(user_input_tfidf).max() * 100
+    
+
+    if confidence_score >= 10:
         response = {"response": predicted_answer}
     else:
-        user_input_tfidf = tfidf_vectorizer.transform([user_input])
-        predicted_answer = random_forest.predict(user_input_tfidf)
-        
-        if selected_option:
-            response = {"response": selected_option}
-        else:
-            options = generate_options(user_input, X_train, tfidf_vectorizer)
-            
-            response = {"response": predicted_answer[0], "options": options}
+        response = {"response": "We are not confident in our answer. Please contact the helpline for assistance."}
+
+    if selected_option:
+        response["selected_option"] = selected_option
+
+    options = generate_options(user_input, X_options, tfidf_vectorizer)
+    response["options"] = options
 
 print(json.dumps(response))
